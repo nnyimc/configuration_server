955be2f427f3d4d2e2ac806357989dfe288bea0b
nnyimc <nmncls@orange.fr>
2020-11-21 03:48:33 +0100

    Minor fixes

---
+++ src/test/java/fr/nnyimc/configuration_server/ConfigurationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.nnyimc.configuration_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
